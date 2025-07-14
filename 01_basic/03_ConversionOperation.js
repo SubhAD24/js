70ea9f15a568f8ffe600286f71d0d8432468a74d
subhra adhikary <164865279+SubhAD24@users.noreply.github.com>
2025-07-14 14:28:09 +0000

    conversion of datatypes

---
+++ 01_basic/03_ConversionOperation.js
@@ -0,0 +1,26 @@
+let score = 44
+
+//console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+
+// "44" => 44
+// "44abc" => NaN
+// true => 1 / false => 0
+
+let isLoggedIn = "subhra" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ // console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+// "subhra" => true
+
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
