f58e2b62dbbf3e2d232bcb45200ffac3c6b4bf1d
subhra adhikary <164865279+SubhAD24@users.noreply.github.com>
2025-07-14 14:11:14 +0000

    Datatye

---
+++ 01_basic/01_Variable.js
@@ -0,0 +1,12 @@
+const accountId = "23456"
+let accountEmail = "Suvroadhikar121@gmail.com"
+var accountPaaword = "sa@1234"
+accountCity = "Bangalore"
+
+// accountId = 2 // NOT ALLOW 
+
+accountEmail = "hfghfc@1234"
+accountPaaword = "64382746328"
+accountCity = "hooghly"
+
+console.table([accountEmail, accountId, accountPaaword, accountCity])
---
+++ 01_basic/02_Datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // treat all js code  as a newer version
+
+// alert(3+3) // WE are using node js, not browser
+
+// NUMBER
+// BIGINT
+// STRING
+// BOOLEAN => true/false
+// null => standlone value
+// undefine => 
+// symbol => unique
+
+
+// OBJECT
+
+
+console.log(typeof undefined); // undefine
+console.log(typeof null); // object
+
